84e2e3fa5d3081dee80793f827a62d60bd5f3350
Kulsum-ahmad <kulsumahmad0703@gmail.com>
2025-07-23 21:33:02 +0530

    Completed Redux store setup and integration

--- src/store.js
+++ src/store.js
@@ -1,4 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
